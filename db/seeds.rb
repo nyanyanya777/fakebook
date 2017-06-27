2d64132dcca1c3d60235fbe5e585a602fcef306b
kohei <kohei@koheinoAir.lan>
2017-06-27 22:26:08 +0900

    commentのseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 
 n = 1
 
-20.times do |n|
-user_id =3
-content = "天気明朗なれど波高し"
-Topic.create!(
-user_id: user_id,
-content: content,
-)
+60.times do
+  user_id = [*1..20].sample
+  topic_id = [*1..20].sample
+  content = Yoshida::Text.sentence
+  Comment.create!(
+    user_id: user_id,
+    topic_id: topic_id,
+    content: content
+  )
+end
 
 end
