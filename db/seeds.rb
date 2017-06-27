2c7c93c903fdea235215fe209f47b6f5caa5e380
kohei <kohei@koheinoAir.lan>
2017-06-28 00:04:37 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
- 100.times do
-   conversation_id = [*1..60].sample
-   user_id = [*1..20].sample
-   body = "猫飼いたい"
-   read = "図鑑が欲しい"
-   Message.create!(
-     conversation_id: conversation_id,
-     user_id: user_id,
-     body: body,
-     read: read
-   )
- end
+50.times do
+  follower_id = [*1..20].sample
+  followed_id = [*1..20].sample
+  while follower_id == followed_id
+    followed_id = [*1..20].sample
+  end
+    Relationship.find_or_create_by(
+    follower_id: follower_id,
+    followed_id: followed_id
+  )
