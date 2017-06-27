2e17780246eb95e457e0e70983a8846511bceea7
kohei <kohei@koheinoAir.lan>
2017-06-27 22:27:59 +0900

    commentのseed修正2

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ n = 1
 60.times do
   user_id = [*1..20].sample
   topic_id = [*1..20].sample
-  content = Yoshida::Text.sentence
+  content = "じゃん"
   Comment.create!(
     user_id: user_id,
     topic_id: topic_id,
