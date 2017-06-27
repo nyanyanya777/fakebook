df176f1f8f8ef18a4dbb9c3c7f35d8616abff159
kohei <kohei@koheinoAir.lan>
2017-06-27 23:52:10 +0900

    会話のseed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-
-n = 1
-
-60.times do
-  user_id = [*1..20].sample
-  topic_id = [*1..20].sample
-  content = "じゃん"
-  Comment.create!(
-    user_id: user_id,
-    topic_id: topic_id,
-    content: content
-  )
-end
+ 100.times do
+   conversation_id = [*1..60].sample
+   user_id = [*1..20].sample
+   body = "猫飼いたい"
+   read = "図鑑が欲しい"
+   Message.create!(
+     conversation_id: conversation_id,
+     user_id: user_id,
+     body: body,
+     read: read
+   )
+ end
