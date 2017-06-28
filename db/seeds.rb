9d03bdcf6efc78955772a8b561a2620795d86338
kohei <kohei@koheinoAir.lan>
2017-06-28 18:13:59 +0900

    コメントデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,11 @@
 n = 1
-100.times do |n|
-  content = Faker::LordOfTheRings.character
-  user_id = 1
-  Topic.create(
-  content: content,
-  user_id: user_id,
-  )
-end
-
-100.times do |n|
-  follower_id = [*1..101].sample
-  followed_id = [*1..101].sample
-  while follower_id == followed_id
-    followed_id = [*1..100].sample
-  end
-    Relationship.find_or_create_by(
-    follower_id: follower_id,
-    followed_id: followed_id
+20.times do |n|
+  user_id = [*1..100].sample
+  topic_id = [*1..100].sample
+  content = "眠い"
+  Comment.create!(
+    user_id: user_id,
+    topic_id: topic_id,
+    content: content
   )
 end
