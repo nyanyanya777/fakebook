e0edad70847b68802a2414e11fa0317104e5bac6
kohei <kohei@koheinoAir.lan>
2017-06-28 18:31:59 +0900

    シード2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+n = 1
+10.times do |n|
+  content = Faker::LordOfTheRings.character
+  user_id = 102
+  Topic.create(
+  content: content,
+  user_id: user_id,
+  )
+end
+
 20.times do
   user_id = [*1..100].sample
   topic_id = [*123..133].sample
@@ -6,15 +16,5 @@
     user_id: user_id,
     topic_id: topic_id,
     content: content
-  )
-end
-
-n = 1
-10.times do |n|
-  content = Faker::LordOfTheRings.character
-  user_id = 102
-  Topic.create(
-  content: content,
-  user_id: user_id,
   )
 end
