67290b1cdaa7154ec8850ea1664e7f8dcfb30e8e
kohei <kohei@koheinoAir.lan>
2017-06-28 18:30:00 +0900

    シード

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
-n = 1
-20.times do |n|
+20.times do
   user_id = [*1..100].sample
-  topic_id = [*1..100].sample
+  topic_id = [*123..133].sample
   content = "眠い"
   Comment.create!(
     user_id: user_id,
     topic_id: topic_id,
     content: content
+  )
+end
+
+n = 1
+10.times do |n|
+  content = Faker::LordOfTheRings.character
+  user_id = 102
+  Topic.create(
+  content: content,
+  user_id: user_id,
   )
 end
