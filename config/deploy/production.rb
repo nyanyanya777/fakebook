a232b1f780a88a7f1e7e21b6aa4785654f3b561c
kohei <kohei@koheinoAir.lan>
2017-06-29 13:48:49 +0900

    祭

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '13.113.157.205', user: 'app', roles: %w{app db web}
+server '52.196.159.192bundle exec rake secret', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/kohei/.ssh/id_rsa'
